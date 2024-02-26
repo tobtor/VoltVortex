a100c7a76ca4a5eb9d12ee03d6101823f70873ab
Pawel <tobtor2@gmail.com>
2024-02-26 23:53:27 +0100

    Add test font and AddProjectWindow

--- app/src/main/java/com/example/voltvortex/MainActivity.java
+++ app/src/main/java/com/example/voltvortex/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.voltvortex;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.addProjectButton);
+
+        Button addProjectButton = findViewById(R.id.addProjectButton);
+        addProjectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddProjectWindow.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
