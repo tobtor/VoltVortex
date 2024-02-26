d7b3ca2f08f6981210d9ce50cca7fc6966805a07
Pawel <tobtor2@gmail.com>
2024-02-26 18:19:01 +0100

    Add button addProject

--- app/src/main/java/com/example/voltvortex/MainActivity.java
+++ app/src/main/java/com/example/voltvortex/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.voltvortex;
 
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.addProjectButton);
     }
 }
