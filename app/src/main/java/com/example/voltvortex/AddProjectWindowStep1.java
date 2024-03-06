145dfdac36f73071d47089fad1d0f9f5aee315ab
Pawel <tobtor2@gmail.com>
2024-03-06 19:16:02 +0100

    Add fonts, second step of AddProject and Project class

--- app/src/main/java/com/example/voltvortex/AddProjectWindowStep1.java
+++ app/src/main/java/com/example/voltvortex/AddProjectWindowStep1.java
@@ -1,5 +1,9 @@
 package com.example.voltvortex;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,23 @@ public class AddProjectWindowStep1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addproject_window_step1);
+
+        Button addProjectButton = findViewById(R.id.buttonZatwierdzProjekt);
+
+        Switch swichIsManyCities = findViewById(R.id.switchDodajProjektPytanie1);
+
+        Switch switchIsManyContactPerson = findViewById(R.id.switchDodajProjektPytanie2);
+
+        Boolean isSingleCity = swichIsManyCities.isChecked();
+        Boolean isSingleContactPerson = switchIsManyContactPerson.isChecked();
+
+        if(isSingleCity) {
+            addProjectButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(AddProjectWindowStep1.this, AddProjectWindowStep2.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/example/voltvortex/Project.java
@@ -0,0 +1,4 @@
+package com.example.voltvortex;
+
+public class Project {
+}
