9ec893c9d8f76f036d4af2735f2c8edf6ae34062
Pawel <tobtor2@gmail.com>
2024-03-06 19:34:24 +0100

    Make the first switch work

--- app/src/main/java/com/example/voltvortex/AddProjectWindowStep1.java
+++ app/src/main/java/com/example/voltvortex/AddProjectWindowStep1.java
@@ -1,8 +1,10 @@
 package com.example.voltvortex;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,20 +18,25 @@ public class AddProjectWindowStep1 extends AppCompatActivity {
 
         Button addProjectButton = findViewById(R.id.buttonZatwierdzProjekt);
 
-        Switch swichIsManyCities = findViewById(R.id.switchDodajProjektPytanie1);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch swichIsManyCities = findViewById(R.id.switchDodajProjektPytanie1);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch switchIsManyContactPerson = findViewById(R.id.switchDodajProjektPytanie2);
 
-        Switch switchIsManyContactPerson = findViewById(R.id.switchDodajProjektPytanie2);
-
-        Boolean isSingleCity = swichIsManyCities.isChecked();
-        Boolean isSingleContactPerson = switchIsManyContactPerson.isChecked();
-
-        if(isSingleCity) {
             addProjectButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    Intent intent = new Intent(AddProjectWindowStep1.this, AddProjectWindowStep2.class);
-                    startActivity(intent);
+
+                    EditText editTextProjectName = findViewById(R.id.editTextNazwaProjektu);
+                    String textProjectName = editTextProjectName.getText().toString();
+                    boolean isSingleCity = swichIsManyCities.isChecked();
+                    boolean isSingleContactPerson = switchIsManyContactPerson.isChecked();
+
+                    Project project = new Project(textProjectName, isSingleCity, isSingleContactPerson);
+
+                    if(project.isSingleCity()) {
+                        Intent intent = new Intent(AddProjectWindowStep1.this, AddProjectWindowStep2.class);
+                        startActivity(intent);
+                    }
                 }
             });
-        }
+
     }
 }
--- app/src/main/java/com/example/voltvortex/Project.java
+++ app/src/main/java/com/example/voltvortex/Project.java
@@ -1,4 +1,18 @@
 package com.example.voltvortex;
 
 public class Project {
+
+    private String projectName;
+    private boolean isSingleCity;
+    private boolean isSingleContactPerson;
+
+    public Project(String projectName, boolean isSingleCity, boolean isSingleContactPerson) {
+        this.projectName = projectName;
+        this.isSingleCity = isSingleCity;
+        this.isSingleContactPerson = isSingleContactPerson;
+    }
+
+    public boolean isSingleCity() {
+        return isSingleCity;
+    }
 }
