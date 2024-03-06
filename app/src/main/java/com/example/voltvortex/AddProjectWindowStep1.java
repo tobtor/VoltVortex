0e38b33f574509be51cb8b1325a998627574bdaa
Pawel <tobtor2@gmail.com>
2024-03-06 18:41:12 +0100

    Complite layout for step 1 addProject

--- app/src/main/java/com/example/voltvortex/AddProjectWindowStep1.java
+++ app/src/main/java/com/example/voltvortex/AddProjectWindowStep1.java
@@ -3,11 +3,11 @@ package com.example.voltvortex;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddProjectWindow extends AppCompatActivity {
+public class AddProjectWindowStep1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addproject_window);
+        setContentView(R.layout.activity_addproject_window_step1);
     }
 }
--- app/src/main/java/com/example/voltvortex/MainActivity.java
+++ app/src/main/java/com/example/voltvortex/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.voltvortex;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupWindow;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         addProjectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddProjectWindow.class);
+                Intent intent = new Intent(MainActivity.this, AddProjectWindowStep1.class);
                 startActivity(intent);
             }
         });
