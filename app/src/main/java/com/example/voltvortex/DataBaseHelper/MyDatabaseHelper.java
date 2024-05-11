6d23c4f17297ef780ac50beaa9165a9744060eb4
Pawel <tobtor2@gmail.com>
2024-05-11 08:51:44 +0200

    Clean and comment MyDatabaseHelper

--- app/src/main/java/com/example/voltvortex/DataBaseHelper/MyDatabaseHelper.java
+++ app/src/main/java/com/example/voltvortex/DataBaseHelper/MyDatabaseHelper.java
@@ -1,36 +1,40 @@
 package com.example.voltvortex.DataBaseHelper;
 
-import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
-import androidx.annotation.Nullable;
-import com.example.voltvortex.DataBaseHelper.CreateTableHelpers.ProjectTableHelper;
-import com.example.voltvortex.DataBaseHelper.CreateTableHelpers.BuildingTableHelper;
-import com.example.voltvortex.DataBaseHelper.CreateTableHelpers.PPARTabelHelper;
-import com.example.voltvortex.DataBaseHelper.CreateTableHelpers.ZSComponentsTableHelper;
-import com.example.voltvortex.DataBaseHelper.CreateTableHelpers.ContactPersonTableHelper;
-import com.example.voltvortex.DataBaseHelper.CreateTableHelpers.ZsElectricalProtectionTableHelper;
+import com.example.voltvortex.DataBaseHelper.CreateTableHelpers.*;
 import com.example.voltvortex.Models.ProjectModel;
 import com.example.voltvortex.Models.BuildingModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Klasa pomocnicza do zarządzania bazą danych.
+ */
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
-    private Context context;
     private static final String DATABASE_NAME = "VoltVortex.db";
     private static final int DATABASE_VERSION = 1;
+    private Context context;
 
-    public MyDatabaseHelper(@Nullable Context context) {
+    /**
+     * Konstruktor klasy pomocniczej bazy danych.
+     * @param context kontekst aplikacji
+     */
+    public MyDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
 
+    /**
+     * Tworzenie tabel w bazie danych.
+     * @param db baza danych
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(PPARTabelHelper.createPPARTable());
@@ -41,17 +45,23 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(BuildingTableHelper.createBuildingTable());
     }
 
+    /**
+     * Aktualizacja bazy danych podczas upgrade'u.
+     * @param db baza danych
+     * @param oldVersion stara wersja bazy danych
+     * @param newVersion nowa wersja bazy danych
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + ProjectTableHelper.getTableName_PROJECT());
-        db.execSQL("DROP TABLE IF EXISTS " + BuildingTableHelper.getTableName_BUILDING());
-        db.execSQL("DROP TABLE IF EXISTS " + ContactPersonTableHelper.getTableName_CONTACT_PERSON());
-        db.execSQL("DROP TABLE IF EXISTS " + ZSComponentsTableHelper.getTableName_ZS_COMPONENT());
-        db.execSQL("DROP TABLE IF EXISTS " + ZsElectricalProtectionTableHelper.getTableName_ZS_ELECTRICAL_PROTECTION());
-        db.execSQL("DROP TABLE IF EXISTS " + PPARTabelHelper.getTableName_PPAR());
         onCreate(db);
     }
 
+    /**
+     * Dodanie nowego projektu do bazy danych.
+     * @param projectModel model projektu
+     * @return true jeśli dodanie się powiodło, false w przeciwnym razie
+     */
     public boolean addProject(ProjectModel projectModel) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
@@ -64,56 +74,38 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
         long insert = db.insert(ProjectTableHelper.getTableName_PROJECT(), null, cv);
         if (insert == -1) {
-            Toast.makeText(context, "Failed to Add Project", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Nie udało się dodać projektu", Toast.LENGTH_SHORT).show();
             return false;
         } else {
-            Toast.makeText(context, "Project Added Successfully", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Projekt dodany pomyślnie", Toast.LENGTH_SHORT).show();
             return true;
         }
     }
 
+    /**
+     * Usunięcie projektu z bazy danych.
+     * @param projectModel model projektu do usunięcia
+     * @return true jeśli usunięcie się powiodło, false w przeciwnym razie
+     */
     public boolean deleteProject(ProjectModel projectModel) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String queryString = "DELETE FROM " + ProjectTableHelper.getTableName_PROJECT() + " WHERE " +
-                ProjectTableHelper.getColumnProjectId() + " = " + projectModel.getProjectID();
+        String queryString = "DELETE FROM " + ProjectTableHelper.getTableName_PROJECT() +
+            " WHERE " + ProjectTableHelper.getColumnProjectId() + " = " + projectModel.getProjectID();
 
-        @SuppressLint("Recycle") Cursor cursor = db.rawQuery(queryString, null);
-
-        if (cursor.moveToFirst()) {
-            return true;
-        } else {
-            return false;
-        }
+        Cursor cursor = db.rawQuery(queryString, null);
+        boolean result = cursor.moveToFirst();
+        cursor.close();
+        return result;
     }
 
-    public boolean addBuilding(BuildingModel buildingModel) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues cv = new ContentValues();
-
-        cv.put(BuildingTableHelper.getColumn_BUILDING_NAME(), buildingModel.getBuildingName());
-        cv.put(BuildingTableHelper.getColumn_DATE_OF_MEASUREMENTS(), buildingModel.getDateOfMeasurements());
-        cv.put(BuildingTableHelper.getColumn_CITY(), buildingModel.getCity());
-        cv.put(BuildingTableHelper.getColumn_POSTCODE(), buildingModel.getPostcode());
-        cv.put(BuildingTableHelper.getColumn_STREET(), buildingModel.getStreet());
-        cv.put(BuildingTableHelper.getColumn_BUILDING_NUMBER(), buildingModel.getBuildingNumber());
-        cv.put(BuildingTableHelper.getColumn_PROJECT_ID(), buildingModel.getProjectID());
-        cv.put(BuildingTableHelper.getColumn_CONTACT_PERSON_ID(), buildingModel.getContactPersonID());
-
-        long insert = db.insert(BuildingTableHelper.getTableName_BUILDING(), null, cv);
-        if (insert == -1) {
-            Toast.makeText(context, "Failed to Add Building", Toast.LENGTH_SHORT).show();
-            return false;
-        } else {
-            Toast.makeText(context, "Building Added Successfully", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-    }
-
+    /**
+     * Pobranie listy wszystkich projektów z bazy danych.
+     * @return lista projektów
+     */
     public List<ProjectModel> viewProjectList() {
         List<ProjectModel> returnList = new ArrayList<>();
 
         String queryString = "SELECT * FROM " + ProjectTableHelper.getTableName_PROJECT();
-
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(queryString, null);
 
@@ -128,7 +120,6 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
                 ProjectModel newProjectModel = new ProjectModel(projectID, projectName, projectFirm, projectDescription, contactPersonID, projectIsSingleContactPerson);
                 returnList.add(newProjectModel);
-
             } while (cursor.moveToNext());
         }
 
