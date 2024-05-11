5a4a54f1cf46fab427886c81248ebca5a97c4ab3
Pawel <tobtor2@gmail.com>
2024-05-11 08:22:06 +0200

    Add PPARModel

---
+++ app/src/main/java/com/example/voltvortex/Models/PPARModel.java
@@ -0,0 +1,30 @@
+package com.example.voltvortex.Models;
+
+public class PPARModel {
+
+    private int pparID;
+    private String content;
+
+    // Konstruktor
+    public PPARModel(int pparID, String content) {
+        this.pparID = pparID;
+        this.content = content;
+    }
+
+    // Gettery i Settery
+    public int getPparID() {
+        return pparID;
+    }
+
+    public void setPparID(int pparID) {
+        this.pparID = pparID;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
