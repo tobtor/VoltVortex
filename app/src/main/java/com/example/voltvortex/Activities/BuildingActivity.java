a8701a39344e92740219e9f92769f5f4ad6e170c
Pawel <tobtor2@gmail.com>
2024-05-27 20:01:58 +0200

    create buttons in BuildingActivity

--- app/src/main/java/com/example/voltvortex/Activities/BuildingActivity.java
+++ app/src/main/java/com/example/voltvortex/Activities/BuildingActivity.java
@@ -1,14 +1,39 @@
 package com.example.voltvortex.Activities;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.voltvortex.AddActivities.AddProjectWindow;
 import com.example.voltvortex.R;
 
 public class BuildingActivity extends AppCompatActivity {
+
+    Button buttonPAR, buttonZS ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building);
+
+        buttonPAR = findViewById(R.id.buttonPAR);
+        buttonZS = findViewById(R.id.buttonZS);
+
+        buttonPAR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BuildingActivity.this, PARActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonZS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BuildingActivity.this, ZSActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
